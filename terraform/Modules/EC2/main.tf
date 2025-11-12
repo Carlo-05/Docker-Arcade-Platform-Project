465b042e2f464600515388c6c8b47b3c4f91fe42
Carlo Ralphael Bravo <104559034+Carlo-05@users.noreply.github.com>
2025-11-12 10:46:51 +0800

    Update main.tf

--- terraform/Modules/EC2/main.tf
+++ terraform/Modules/EC2/main.tf
@@ -4,10 +4,11 @@ resource "aws_instance" "ec2_practice" {
     vpc_security_group_ids = [var.ec2_practice_SG]
     subnet_id = var.ec2_practice_subnet
     key_name = var.key_name
-    user_data = file("${path.root}/documents/docker-deploy.sh")
+    user_data = file("${path.root}/../../documents/docker-deploy.sh")
 
     # Tags
     tags = merge(var.default_tags, {Name = var.ec2_practice_tag})
 
 
 }
+
